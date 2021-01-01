f13cedc87168190e4e5fcc0c1575588a3151e286
Mike Bruty <michael.bruty@students.plymouth.ac.uk>
2021-01-01 14:35:32 +0000

    Implementing IModel

--- src/com/model/StockModel.java
+++ src/com/model/StockModel.java
@@ -1,6 +1,12 @@
-package com.stock;
+package com.model;
 
-import java.io.*;
+import com.controller.AbstractController;
+import com.stock.StockItem;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 import java.nio.charset.StandardCharsets;
@@ -8,12 +14,33 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Stock {
+public class StockModel implements IModel{
+
+    private final List<AbstractController> observers = new ArrayList<>();
+    private final List<StockItem> stock = new ArrayList<>();
+    @Override
+    public void subscribe(AbstractController observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void unSubscribe(AbstractController observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void onChange(List<StockItem> item) {
+        for (AbstractController controller:
+             observers) {
+            controller.updateView(stock);
+        }
+    }
+
     private final List<StockItem> items = new ArrayList<>();
     private final String path = "resources/stock.csv";
     private String header;
 
-    public Stock() {
+    public StockModel() {
         try {
             File stockFile = new File(path);
             Scanner reader = new Scanner(stockFile);
@@ -29,10 +56,6 @@ public class Stock {
         }
     }
 
-    public void add(StockItem item) {
-        items.add(item);
-    }
-
     public void writeToFile() {
         try (FileOutputStream fileOutputStream = new FileOutputStream(path);
              FileChannel channel = fileOutputStream.getChannel();
@@ -40,7 +63,7 @@ public class Stock {
             Thread.sleep(5000);
             fileOutputStream.write(header.getBytes(StandardCharsets.UTF_8));
             for (StockItem item:
-                 items) {
+                    items) {
                 fileOutputStream.write(
                         item.toCommaDelimited()
                                 .getBytes(StandardCharsets.UTF_8)
