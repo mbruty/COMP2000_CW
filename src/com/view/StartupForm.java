09c7a5f4438437105f5ba10cebb588e4210de448
Mike Bruty <michael.bruty@students.plymouth.ac.uk>
2021-01-04 13:24:52 +0000

    Added log-in ui with no functionality

--- src/com/view/StartupForm.java
+++ src/com/view/StartupForm.java
@@ -1,25 +1,40 @@
 package com.view;
 
+import com.KeyValuePair;
+import com.controller.AbstractController;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class StartupForm extends JFrame{
-    private JPanel mainPanel;
-    private JButton adminButton;
-    private JButton pointOfSalesButton;
+public class StartupForm extends AbstractView {
+  private JPanel mainPanel;
+  private JButton adminButton;
+  private JButton pointOfSalesButton;
+  private JPasswordField passwordField1;
+  private JTextField textField1;
+  private AbstractController controller;
 
-    public StartupForm(AbstractView admin, AbstractView pointOfSale) {
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setContentPane(mainPanel);
-        this.pack();
-        this.setVisible(true);
-        adminButton.addActionListener(e -> {
-            admin.setVisible(true);
-            this.setVisible(false);
-        });
-        pointOfSalesButton.addActionListener(e -> {
-            pointOfSale.setVisible(true);
-            this.setVisible(false);
-        });
-    }
+  public StartupForm(AbstractView admin, AbstractView pointOfSale) {
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    this.setContentPane(mainPanel);
+    this.pack();
+    this.setVisible(true);
+    adminButton.addActionListener(e -> {
+      admin.setVisible(true);
+      this.setVisible(false);
+    });
+    pointOfSalesButton.addActionListener(e -> {
+      pointOfSale.setVisible(true);
+      this.setVisible(false);
+    });
+  }
+
+  @Override
+  public void update(KeyValuePair item) {
+  }
+
+  @Override
+  public void setController(AbstractController controller) {
+    this.controller = controller;
+  }
 }
