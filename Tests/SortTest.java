8e5efe0b535acdc301d6586a92d209e08dfec6c5
Mike Bruty <michael.bruty@students.plymouth.ac.uk>
2021-01-04 13:20:00 +0000

    String sort and tests for string sort

--- Tests/SortTest.java
+++ Tests/SortTest.java
@@ -1,4 +1,10 @@
-package tests;
+import com.Utils.Sort;
+import com.Utils.SortInteger;
+import com.Utils.SortString;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,5 +16,33 @@ class SortTest {
 
   @org.junit.jupiter.api.Test
   void doSort() {
+    // Class Integer over int for the type as primitive types cannot be given to generics
+    final int nItems = 100000;
+    Integer[] sortMe = new Integer[nItems];
+    Random rand = new Random();
+    for(int i = 0; i < nItems; i++) {
+      sortMe[i] = rand.nextInt();
+    }
+    Sort<Integer> sort = new Sort<>(new SortInteger(), sortMe);
+    sort.doSort();
+    assertTrue(isSorted(sortMe));
+  }
+
+  @org.junit.jupiter.api.Test
+  void doSortWithString(){
+    String[] sortMe = new String[]{
+      "aa", "X", "b", "x", "a", "A",
+    };
+    Sort<String> sort = new Sort<>(new SortString(), sortMe);
+    sort.doSort();
+    assertArrayEquals(new String[]{"A", "a", "aa", "b", "X", "x"}, sortMe);
+  }
+  boolean isSorted(Integer[] array) {
+    for (int i = 0; i < array.length - 2; i++) {
+      if(array[i] > array[i + 1]) {
+        return false;
+      }
+    }
+    return true;
   }
 }
