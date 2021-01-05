7acef76df9bfadd426e35be79bdbedb701851b35
Mike Bruty <michael.bruty@students.plymouth.ac.uk>
2021-01-05 10:42:45 +0000

    Boilerplate for admin model

--- src/com/model/AdminModel.java
+++ src/com/model/AdminModel.java
@@ -1,2 +1,32 @@
-package com.model;public class AdminModel {
+package com.model;
+
+import com.KeyValuePair;
+import com.controller.AbstractController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminModel implements IModel{
+  private List<AbstractController> observers = new ArrayList<>();
+
+  private String userName;
+  private String password;
+
+  @Override
+  public void subscribe(AbstractController observer) {
+    observers.add(observer);
+  }
+
+  @Override
+  public void unSubscribe(AbstractController observer) {
+    observers.remove(observer);
+  }
+
+  @Override
+  public void onChange(KeyValuePair item) {
+    for (AbstractController observer:
+         observers) {
+      observer.updateView(item);
+    }
+  }
 }
