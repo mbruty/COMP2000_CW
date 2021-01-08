138bf5ee0a85176961cf09546394a38bef3b40d2
Mike Bruty <michael.bruty@students.plymouth.ac.uk>
2021-01-08 11:15:16 +0000

    Hashing and checking hashed password

--- src/com/model/AdminModel.java
+++ src/com/model/AdminModel.java
@@ -2,6 +2,7 @@ package com.model;
 
 import com.KeyValuePair;
 import com.controller.AbstractController;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +12,16 @@ public class AdminModel implements IModel{
 
   private String userName;
   private String password;
+  private boolean isAdmin;
+
+  public AdminModel(String commaDelimitedLine) {
+    String[] items = commaDelimitedLine.split(",");
+    this.userName = items[0];
+    this.password = items[1];
+    this.isAdmin = Boolean.parseBoolean(items[2]);
+  }
+
+  public AdminModel() { }
 
   @Override
   public void subscribe(AbstractController observer) {
@@ -28,5 +39,36 @@ public class AdminModel implements IModel{
          observers) {
       observer.updateView(item);
     }
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public void hashAndSetPassword(String password)  {
+    this.password = BCrypt.hashpw(password, BCrypt.gensalt(12));
+  }
+
+  public boolean validatePassword(String password) {
+    return BCrypt.checkpw(password, this.password);
+  }
+  public boolean isAdmin() {
+    return isAdmin;
+  }
+
+  public void setAdmin(boolean admin) {
+    isAdmin = admin;
   }
 }
