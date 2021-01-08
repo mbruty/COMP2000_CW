a4c284238a4f05fca2af5b4bcdf10effbcb8744c
Mike Bruty <michael.bruty@students.plymouth.ac.uk>
2021-01-08 14:33:02 +0000

    Login validation and loading new view

--- src/com/view/StartupForm.java
+++ src/com/view/StartupForm.java
@@ -2,10 +2,10 @@ package com.view;
 
 import com.KeyValuePair;
 import com.controller.AbstractController;
+import com.controller.AdminController;
 import com.controller.MultiAdminController;
 
 import javax.swing.*;
-import java.util.Arrays;
 
 public class StartupForm extends AbstractView {
   private JPanel mainPanel;
@@ -15,9 +15,9 @@ public class StartupForm extends AbstractView {
   private JTextField userNameTf;
   private JLabel errorTxt;
   private MultiAdminController controller;
-  private AbstractView admin;
+  private Admin admin;
 
-  public StartupForm(AbstractView admin, AbstractView pointOfSale) {
+  public StartupForm(Admin admin, AbstractView pointOfSale) {
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     this.admin = admin;
     this.setContentPane(mainPanel);
@@ -25,7 +25,14 @@ public class StartupForm extends AbstractView {
     this.center();
     this.setVisible(true);
     adminButton.addActionListener(e -> {
-      controller.checkLogin(userNameTf.getText(), new String( passwordField.getPassword()));
+      new SwingWorker<>() {
+
+        @Override
+        protected Object doInBackground() throws Exception {
+          controller.checkLogin(userNameTf.getText(), new String( passwordField.getPassword()));
+          return null;
+        }
+      }.execute();
     });
     pointOfSalesButton.addActionListener(e -> {
       pointOfSale.setVisible(true);
@@ -34,13 +41,14 @@ public class StartupForm extends AbstractView {
   }
   @Override
   public void update(KeyValuePair item) {
-    if ("Validate".equals(item.key)) {
-      if (item.value.equals("Validated")) {
-        admin.setVisible(true);
-        this.setVisible(false);
-      } else {
-        errorTxt.setText((String) item.value);
-      }
+    if (item.key.equals("Validate")) {
+      errorTxt.setText((String) item.value);
+    } else if (item.key.equals("Validated")) {
+      AdminController newController = (AdminController) item.value;
+      newController.setView(admin);
+      admin.setAdminController(newController);
+    } else if (item.key.equals("isAdmin")) {
+      admin.setNotAdmin();
     }
   }
 
