7b7f4e3193203433c1d66ced22a29903477b9fd7
Mike Bruty <michael.bruty@students.plymouth.ac.uk>
2021-01-14 15:13:39 +0000

    Auto generated artefact stuff

---
+++ Tests/com/controller/MockItemModel.java
@@ -0,0 +1,36 @@
+package com.controller;
+
+import com.KeyValuePair;
+import com.model.IModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockItemModel implements IModel {
+
+  private List<AbstractController> observers = new ArrayList<>();
+
+  public MockItemModel() {
+
+  }
+
+  @Override
+  public void subscribe(AbstractController observer) {
+
+  }
+
+  @Override
+  public void unSubscribe(AbstractController observer) {
+
+  }
+
+  @Override
+  public void onChange(KeyValuePair item) {
+
+  }
+
+  @Override
+  public boolean writeToFile() {
+    return false;
+  }
+}
---
+++ Tests/com/controller/StockControllerTest.java
@@ -0,0 +1,22 @@
+package com.controller;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StockControllerTest {
+
+
+  @Before
+  void setup() {
+
+  }
+  @Test
+  void getItemByCode() {
+  }
+
+  @Test
+  void writeFile() {
+  }
+}
